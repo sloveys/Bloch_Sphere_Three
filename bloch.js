2e64e11600af63ef3d5755ac49b022e8c15d73c7
sloveys <samuel_loveys@outlook.com>
2018-12-01 14:14:24 -0500

    sphere is now working!

--- bloch.js
+++ bloch.js
@@ -1,17 +1,17 @@
-/*global window, THREE, document, Uint8Array*/
-/*jslint indent: 2 */
 var width = window.innerWidth;
 var height = window.innerHeight;
 var viewAngle = 75;
 var nearClipping = 0.1;
 var farClipping = 1000;
+
 var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+
 var camera = new THREE.PerspectiveCamera(viewAngle, width / height, nearClipping, farClipping);
 
-var renderer = new THREE.WebGLRenderer();
-//renderer.setClearColor(0xFFFFFF);
+var renderer = new THREE.WebGLRenderer({antialias: true});
 
-renderer.setPixelRatio(window.devicePixelRatio / 2);
+renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -21,33 +21,38 @@ controls.minPolarAngle = blockedAngle;
 controls.maxPolarAngle = Math.PI - blockedAngle;
 controls.minDistance = 1.2;
 controls.maxDistance = 4;
-camera.position.set(4, 0, 0);
+camera.position.set(2, 0, 0);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 controls.update();
 
 function genSphere() {
-  //"use strict";
-  var txtrHW = 1000;
+  var txtrWidth = 1280;
+  var txtrHeight = 720;
   var sphereGeometry = new THREE.SphereGeometry(1.0, 64, 64);
 
-  var txtrData = new Uint8Array(4 * txtrHW*txtrHW);
-  for (var w = 0; w < txtrHW; w++) {
-    for (var h = 0; h < txtrHW; h++) {
-    	var stride = (h * txtrHW + w) * 4;
-    	txtrData[stride] = 200; // r; rgb is 0...255
-    	txtrData[stride + 1] = 200; // g
-      txtrData[stride + 2] = 200; // b
-      txtrData[stride + 3] = 200; // a
+  var txtrData = new Uint8Array(4 * txtrWidth * txtrHeight);
+  for (var w = 0; w < txtrWidth; w++) {
+    for (var h = 0; h < txtrHeight; h++) {
+    	var stride = ((h * txtrWidth) + w) * 4;
+    	txtrData[stride] = 255; // r; rgb is 0...255
+    	txtrData[stride + 1] = 255; // g
+      txtrData[stride + 2] = 255; // b
+      if (w % 40 && h % 40) {
+        txtrData[stride + 3] = 0; // a
+      } else {
+        txtrData[stride + 3] = 255; // a
+      }
     }
   }
 
-  var sphereTexture = new THREE.DataTexture(txtrData, txtrHW, txtrHW, THREE.RGBAFormat);
+  var sphereTexture = new THREE.DataTexture(txtrData, txtrWidth, txtrHeight, THREE.RGBAFormat);
+  sphereTexture.needsUpdate = true;
   var sphereMaterial = new THREE.MeshPhongMaterial({
     map: sphereTexture,
     shininess: 0.0,
     transparent: true});
-  //var testMat = new MeshBasicMaterial();
-  //testMat.color(0xffffff);
+  var testMat = new THREE.MeshBasicMaterial();
+  testMat.color = new THREE.Color(0x00FF00);
   var local_sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
   local_sphere.position.z = 0;
   local_sphere.position.x = 0;
