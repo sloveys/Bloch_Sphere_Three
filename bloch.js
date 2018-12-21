570e067c41d83bf7501a95c770c28c36ead9b1be
sloveys <samuel_loveys@outlook.com>
2018-12-21 13:22:03 -0500

    Cleaned up lots of code

--- bloch.js
+++ bloch.js
@@ -1,31 +1,92 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
-var viewAngle = 75;
-var nearClipping = 0.1;
-var farClipping = 1000;
+var loader, scene, camera, renderer, controls,
+  sphere, xAxis, yAxis, zAxis, ket0, ket1, states;
 
-var loader = new THREE.FontLoader();
 
-var scene = new THREE.Scene();
-scene.background = new THREE.Color(0x202020);
+function init_bloch_sphere(containerName='Bloch Sphere') {
+  var viewAngle = 75;
+  var nearClipping = 0.1;
+  var farClipping = 1000;
 
-var camera = new THREE.PerspectiveCamera(viewAngle, width / height, nearClipping, farClipping);
+  var container = document.getElementById(containerName);
 
-var renderer = new THREE.WebGLRenderer({antialias: true});
+  loader = new THREE.FontLoader();
 
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x202020);
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enablePan = false;
-var blockedAngle = 0.25;
-controls.minPolarAngle = blockedAngle;
-controls.maxPolarAngle = Math.PI - blockedAngle;
-controls.minDistance = 1.2;
-controls.maxDistance = 4;
-camera.position.set(1.1, 1.1, 1.3);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-controls.update();
+  camera = new THREE.PerspectiveCamera(viewAngle, window.innerWidth / window.innerHeight, nearClipping, farClipping);
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enablePan = false;
+  var blockedAngle = 0.25;
+  controls.minPolarAngle = blockedAngle;
+  controls.maxPolarAngle = Math.PI - blockedAngle;
+  controls.minDistance = 1.2;
+  controls.maxDistance = 4;
+  camera.position.set(1.1, 1.1, 1.3);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  controls.update();
+
+  sphere = genSphere();
+
+  xAxis = genLine(new THREE.Vector3(1, 0, 0), 0x44FF44, 'x');
+  yAxis = genLine(new THREE.Vector3(0, 0, 1), 0xFF4444, 'y');
+  zAxis = genLine(new THREE.Vector3(0, 1, 0), 0x4444FF, 'z');
+
+  loader.load('three.js-master/examples/fonts/droid/droid_serif_regular.typeface.json', function (font) {
+    var ket0Geometry = new THREE.TextGeometry('|0>', {
+      font: font,
+      size: 0.1,
+      height: 0.01 });
+    var ket1Geometry = new THREE.TextGeometry('|1>', {
+      font: font,
+      size: 0.1,
+      height: 0.01 });
+    var ketMaterial = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
+    ket0 = new THREE.Mesh(ket0Geometry, ketMaterial);
+    ket0.position.set(0, 1.1, 0);
+    scene.add(ket0);
+    ket1 = new THREE.Mesh(ket1Geometry, ketMaterial);
+    ket1.position.set(0, -1.2, 0);
+    scene.add(ket1);
+  });
+
+  var ambiLight = new THREE.AmbientLight(0xFFFFFF);
+  scene.add(ambiLight);
+
+  states = [];
+  addState(1, 1, 1, 0xFFFF00, "|a>");
+  addState(1, 0, -3, 0x00FFFF, "|b>");
+
+  window.addEventListener('resize', onWindowResize, false);
+
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  ket0.lookAt(camera.position);
+  ket1.lookAt(camera.position);
+  xAxis.stateName.lookAt(camera.position);
+  yAxis.stateName.lookAt(camera.position);
+  zAxis.stateName.lookAt(camera.position);
+  for (var i=0; i<states.length; i++) {
+    states[i].stateName.lookAt(camera.position);
+  }
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 function genSphere() {
   var txtrHW = 360;
@@ -61,7 +122,6 @@ function genSphere() {
   scene.add(local_sphere);
   return local_sphere;
 }
-var sphere = genSphere();
 
 function genLine(position, lineColor, name) {
   var lineMaterial = new THREE.LineBasicMaterial({color: lineColor});
@@ -87,39 +147,13 @@ function genLine(position, lineColor, name) {
 
   return line;
 }
-var xAxis = genLine(new THREE.Vector3(1, 0, 0), 0x44FF44, 'x');
-var yAxis = genLine(new THREE.Vector3(0, 0, 1), 0xFF4444, 'y');
-var zAxis = genLine(new THREE.Vector3(0, 1, 0), 0x4444FF, 'z');
 
-var ket0, ket1;
-
-loader.load('three.js-master/examples/fonts/droid/droid_serif_regular.typeface.json', function (font) {
-  var ket0Geometry = new THREE.TextGeometry('|0>', {
-    font: font,
-    size: 0.1,
-    height: 0.01 });
-  var ket1Geometry = new THREE.TextGeometry('|1>', {
-    font: font,
-    size: 0.1,
-    height: 0.01 });
-  var ketMaterial = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
-  ket0 = new THREE.Mesh(ket0Geometry, ketMaterial);
-  ket0.position.set(0, 1.1, 0);
-  scene.add(ket0);
-  ket1 = new THREE.Mesh(ket1Geometry, ketMaterial);
-  ket1.position.set(0, -1.2, 0);
-  scene.add(ket1);
-});
-
-var ambiLight = new THREE.AmbientLight(0xFFFFFF);
-scene.add(ambiLight);
-
-var states = [];
 function addState(x, y, z, color, name) {
   var vec = new THREE.Vector3(x, z, y); // Three.js swaps y and z for some reason
   vec.normalize();
   states.push(genLine(vec, color, name));
 }
+
 /*function removeState(name) {
   for (var i=0; i<states.length; i++) {
     if (states[i].name == name) {
@@ -130,20 +164,3 @@ function addState(x, y, z, color, name) {
     }
   }
 }*/
-addState(1, 1, 1, 0xFFFF00, "|a>");
-addState(1, 0, -3, 0x00FFFF, "|b>");
-
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  ket0.lookAt(camera.position);
-  ket1.lookAt(camera.position);
-  xAxis.stateName.lookAt(camera.position);
-  yAxis.stateName.lookAt(camera.position);
-  zAxis.stateName.lookAt(camera.position);
-  for (var i=0; i<states.length; i++) {
-    states[i].stateName.lookAt(camera.position);
-  }
-  renderer.render(scene, camera);
-}
-animate();
